52f67404b443c2ce00b2b12a6f6f88cc6c3516f1
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-07-15 19:59:22 -0400

    Fix: Aiming was not correct
    Now its in the right pos. If i do this again, you need to move the shot pos of where the front of the gun is! I dont have a art assets for it but the capsule will work.

--- 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayerRangeWeapon.cs
+++ 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayerRangeWeapon.cs
@@ -11,7 +11,7 @@ public class PlayerRangeWeapon : MonoBehaviour
     {
         //This deals with rotating weapon below
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotz = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
+        float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotz + Offset);
 
         if(Input.GetMouseButtonDown(0))
--- 2D_TurnBased_Template/Assets/Code/PlayerCode/Projectile.cs
+++ 2D_TurnBased_Template/Assets/Code/PlayerCode/Projectile.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float Speed;
-    public float LifeTime;
-    public float Distance;
+    [Header("Projectiles info")]
+    public float SpeedOfProjectile;
+    public float LifeTimeOfProjectile;
+    public float DistanceOfProjectile;
+    [Header("Damage")]
     public int RangeDamage;
+    [Header("Layers")]
     public LayerMask Enemy;
     private void Start()
     {
-        Invoke("DestroyProjectile", LifeTime);
+        Invoke("DestroyProjectile", LifeTimeOfProjectile);
     }
 
     private void Update()
     {
-
-        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.up, Distance);
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.up, DistanceOfProjectile);
         if(hitinfo.collider != null)
         {
             if (hitinfo.collider.CompareTag("Enemy"))
@@ -26,7 +28,7 @@ public class Projectile : MonoBehaviour
             DestroyProjectile();
         }
         
-        transform.Translate(transform.up * Speed * Time.deltaTime);
+        transform.Translate(Vector2.up * SpeedOfProjectile * Time.deltaTime);
     }
     void DestroyProjectile()
     {
