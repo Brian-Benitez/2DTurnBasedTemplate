99ba8967b6931746eaa8b58c27b5f28d55b79395
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-07-22 20:46:35 -0400

    Made/ updated: players info
    I used the base class for other characters for the player now. So I dont have to write more stuff.

--- 2D_TurnBased_Template/Assets/Code/Base Classes/BaseCharacter.cs
+++ 2D_TurnBased_Template/Assets/Code/Base Classes/BaseCharacter.cs
@@ -1,20 +1,46 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class BaseCharacter : MonoBehaviour
 {
+    [Header("Name")]
     public string NameOfCharacter;
-
+    [Header("Health")]
     public int CharacterHealthAmount;
     public int CharacterMaxHealthLevel;
-
+    [Header("Damage")]
     public int CharacterDamageOutput;
-
+    [Header("Sanity")]
     public int CharacterSanityAmount;
     public int CharacterMaxSanityLevel;
 
     public void PrintInfo()
     {
         Debug.Log("Name: " + NameOfCharacter + " Max health:" + CharacterMaxHealthLevel + " Max Sainty: " + CharacterMaxSanityLevel);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CharacterHealthAmount -= damage;
+        Debug.Log(NameOfCharacter + " took: " + damage);
+        DoesCharacterDie();
+    }
+
+    public void DoesCharacterDie()
+    {
+        if (CharacterHealthAmount < 0)
+        {
+            this.gameObject.gameObject.SetActive(false);
+        }
+        else
+            Debug.Log(NameOfCharacter + " Still has health");
+    }
+
+    public void Delay(float time, System.Action _callBack)
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.AppendInterval(time).AppendCallback(() => _callBack());
     }
 
 }
