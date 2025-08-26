3eddc3c075d5b09e81d666a2403dbd11a9d2857c
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-08-25 22:05:22 -0400

    Created: Get within range attack
    Still not sure if it works just writing all the code right now

--- 2D_TurnBased_Template/Assets/Code/States/RangeStates/GetWithinRangeAttackState.cs
+++ 2D_TurnBased_Template/Assets/Code/States/RangeStates/GetWithinRangeAttackState.cs
@@ -2,8 +2,49 @@ using UnityEngine;
 
 public class GetWithinRangeAttackState : State
 {
+    [Header("Is Within Range")]
+    public bool WithinRangeAttack = false;
+    [Header("Stopping distance from enemy")]
+    public float StoppingDistanceForRangeAttack;
+    [Header("How far target is")]
+    public float DistanceFromTarget;
+    float _maxDistanceFromTarget;
+    //States below
+    RangeAttackState RangeAttackState;
+    TargetARangeEnemyState TargetARangeEnemyState;
+    BaseEnemy BaseEnemy;
+
+    private void Start()
+    {
+        RangeAttackState = GetComponent<RangeAttackState>();
+        TargetARangeEnemyState = GetComponent<TargetARangeEnemyState>();
+        BaseEnemy = GetComponent<BaseEnemy>();
+        _maxDistanceFromTarget = DistanceFromTarget;
+    }
+
+    private void Update()
+    {
+        if(WithinRangeAttack)
+            return;
+        if (DistanceFromTarget > StoppingDistanceForRangeAttack)
+        {
+            TurnOffWithinRangeBool();
+            DistanceFromTarget = Vector2.Distance(transform.position, TargetARangeEnemyState.CurrentRangeTarget.transform.position);
+            Vector2 direction = TargetARangeEnemyState.CurrentRangeTarget.transform.position - transform.position;
+            transform.position = Vector2.MoveTowards(this.transform.position, TargetARangeEnemyState.CurrentRangeTarget.transform.position, BaseEnemy.EnemySpeed * Time.deltaTime);
+        }
+        else
+            TurnOnWithinRangeBool();
+    }
     public override State RunCurrentState()
     {
+        if (WithinRangeAttack)
+            return RangeAttackState;
         return this;
     }
+    //Helper functions-------------------------------------------------------->
+
+    public void RestartDisanceFromTarget() => DistanceFromTarget = _maxDistanceFromTarget;
+    public void TurnOffWithinRangeBool() => WithinRangeAttack = false;
+    public void TurnOnWithinRangeBool() => WithinRangeAttack = true;
 }
