dc999c934931ba0d0908fad1d62105c6a61decfd
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-10-12 17:51:19 -0400

    Made: Player can now dash
    I had to unhook the pausing player for now, make sure we fix that next update

--- 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayerMovement.cs
+++ 2D_TurnBased_Template/Assets/Code/PlayerCode/PlayerMovement.cs
@@ -1,26 +1,66 @@
+using System.Collections;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float PlayerSpeed;
     public GameObject PlayerObject;
+    public Rigidbody2D Rb;
 
     public bool StopPlayerMovement = false;
 
+    [Header("Dash Settings")]
+    public float DashSpeed = 10f;
+    public float DashDuration = 1f;
+    public float DashCoolDown = 1f;
+    public bool IsDashing;
+    public bool CanDash = true;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+
     private void Update()
     {
+        if (IsDashing)
+            return;
+
         if(StopPlayerMovement)
             return;
-        else
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && CanDash)
         {
-            float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
-
-            Vector3 tempVect = new Vector3(x, y, 0);
-            tempVect = tempVect.normalized * PlayerSpeed * Time.deltaTime;
-
-            PlayerObject.transform.position += tempVect;
+            Debug.Log("dash");
+            StartCoroutine(Dash());
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsDashing)
+            return;
+
+        Rb.linearVelocity  = new Vector2(moveDirection.x * PlayerSpeed, moveDirection.y * PlayerSpeed);
+        //Vector2 aimdirection = mousePosition - Rb.position;
+        //float aimAngle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg - 90f;
+        //Rb.rotation = aimAngle;
+    }
+
+    public IEnumerator Dash()
+    {
+        CanDash = false;
+        IsDashing = true;
+        Rb.linearVelocity = new Vector2(moveDirection.x * DashSpeed, moveDirection.y * DashSpeed);
+        yield return new WaitForSeconds(DashDuration);
+        IsDashing = false;  
+        yield return new WaitForSeconds(DashCoolDown);
+        CanDash = true;
     }
 
     public void TurnOnStopPlayerMovement() => StopPlayerMovement = true;
